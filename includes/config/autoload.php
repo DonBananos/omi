3f05dc71846a4a691d330f1b5aa0f6ba5c672edf
Mike Jensen <mikejensen2@gmail.com>
2015-03-27 10:52:55 +0100

    Some CSS, config files created, navbar added.

---
+++ includes/config/autoload.php
@@ -0,0 +1,4 @@
+<?php
+
+//As for now, this file is not used. 
+//I will try to get this to work
--- index.php
+++ index.php
@@ -1,16 +1,22 @@
+<?php
+require './includes/config/config.php';
+?>
+
 <!DOCTYPE html>
-<!--
-Index file of onlineMovieIndex project
--->
 <html>
     <head>
-        <meta charset="UTF-8">
-        <title>Online Movie Index</title>
+		<title>Online Movie Index</title>
+		<?php
+		require './includes/header.html';
+		?>
     </head>
     <body>
-		<?php
-		echo '<h1>Online Movie Index (OMI)</h1>';
-		echo 'Today is: <b>'.date('l jS F Y').'</b>'; //l : day of week, j = daynumber in month no 0 in front, S is ordinal suffix for date, F is month name, Y is year
-		?>
+		<div class="container">
+			<?php
+			require './includes/navbar.php';
+			echo '<h1>Online Movie Index (OMI)</h1>';
+			echo 'Today is: <b>'.date('l jS F Y').'</b>'; //l : day of week, j = daynumber in month no 0 in front, S is ordinal suffix for date, F is month name, Y is year
+			?>
+		</div>
     </body>
 </html>
