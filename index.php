ba431e44e940cf35dee62a82f44d51208656f7a0
Mike Jensen <mikejensen2@gmail.com>
2015-03-27 11:21:36 +0100

    Minor css changes

--- index.php
+++ index.php
@@ -11,11 +11,17 @@ require './includes/config/config.php';
 		?>
     </head>
     <body>
-		<div class="container">
+		<?php
+		require './includes/navbar.php';
+		?>
+		<div class="container page-wrap">
+			<div id="col-lg-12">
+            <div class="page-header">
+                <h1>Welcome to Online Movie Index</h1>
+            </div>
+        </div>
 			<?php
-			require './includes/navbar.php';
-			echo '<h1>Online Movie Index (OMI)</h1>';
-			echo 'Today is: <b>'.date('l jS F Y').'</b>'; //l : day of week, j = daynumber in month no 0 in front, S is ordinal suffix for date, F is month name, Y is year
+			echo '<small>Today is: <b>'.date('l jS F Y').'</b></small>'; //l : day of week, j = daynumber in month no 0 in front, S is ordinal suffix for date, F is month name, Y is year
 			?>
 		</div>
     </body>
