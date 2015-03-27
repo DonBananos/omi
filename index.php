ae52d4e1840650c5ea23a12c3735743829696a32
Mike Jensen <mikejensen2@gmail.com>
2015-03-27 09:26:37 +0100

    Initial Commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<!--
+Index file of onlineMovieIndex project
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Online Movie Index</title>
+    </head>
+    <body>
+		<?php
+		$now = time();
+		echo $now;
+		?>
+    </body>
+</html>
