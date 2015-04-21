e7299ff8b0d15a5d2c52351be6b6f32f1b309914
HLO <HLO@10.111.180.123>
2015-04-21 12:47:28 +0200

    Angular app impl. Search + details

--- includes/memberStart.php
+++ includes/memberStart.php
@@ -3,12 +3,15 @@ Author: Heini L. Ovason
 -->
 
 <div class="container">
-    <div class="row">
+    <div class="row"> 
 
         <h1>Welcome Back <?php echo $active_user->getUsername() ?></h1>
 
+    </div>
+    <div class="row"> 
         <?php require './collection/createCollectionView.php'; ?>
-        
-
+    </div>
+    <div class="row"> 
+        <?php require './app/index.html'; ?>
     </div>
 </div>
