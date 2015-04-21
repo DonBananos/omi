e2b45ab91ae10593f6f9f8ec4983f8288b8b60a4
Mike Jensen <mikejensen2@gmail.com>
2015-04-21 12:36:40 +0200

    User Credentials Validation Fix

---
+++ user/register.php
@@ -0,0 +1,32 @@
+<?php
+//This file should only be used to register a user.. Should receive Username, Email and password.
+
+require 'database.php';
+require 'config.php';
+require '../../user/user.php';
+
+$username = $_POST['username'];
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+$user = new User();
+$answer = $user->createUser($username, $email, $password);
+if($answer)
+{
+	?>
+<script>
+	alert('Welcome to Omi');
+	window.location = '<?php echo $path ?>';
+</script>
+
+	<?php
+}
+else
+{
+	?>
+<script>
+	alert('SQL Error:<?php echo $answer ?>');
+	window.location = '<?php echo $path ?>';
+</script>
+	<?php
+}
