b3f6c1258a6103cf2af0e56b5c0f9e28143905c7
DonBananos <mikejensen2@gmail.com>
2016-05-28 02:28:33 +0200

    Member start remake... Still not nearly finished though!

--- includes/footer.php
+++ includes/footer.php
@@ -12,7 +12,7 @@ Author: R. Mike Jensen, Heini L. Ovason
 </div> <!-- END .main-container (BEGIN <div> is located in header.php) -->
 
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+<script src="<?php echo $path ?>includes/jquery/jquery-1.11.3.min.js"></script>
 
 <!-- Bootstrap JS -->
 <script src="<?php echo $path ?>js/bootstrap.min.js"></script>
--- includes/header.php
+++ includes/header.php
@@ -14,7 +14,7 @@ Author: R. Mike Jensen, Heini L. Ovason
 <!-- Bootstrap CSS Social -->
 <link rel="stylesheet" href="<?php echo $path ?>css/bootstrap-social.css">
 <!-- Latest Font Awesome -->
-<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="<?php echo $path ?>includes/font-awesome-4.5.0/css/font-awesome.min.css">
 <link rel="shortcut icon" href="<?php echo $path ?>includes/img/favicon.ico">
 <meta name="theme-color" content="#333">
 <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
